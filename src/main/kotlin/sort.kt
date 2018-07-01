8b198b7e1499106c7256fbec922010fcee6fe411
Nazmul Idris <nazmul@fasterlap.com>
2018-07-01 13:28:12 -0700

    Add Merge Sort implementation

--- src/main/kotlin/sort.kt
+++ src/main/kotlin/sort.kt
@@ -41,7 +41,62 @@ fun main(args: Array<String>) {
         println(", $stats")
     }
 
+    // merge sort
+    with(RuntimeStats()) {
+        println("merge_sort".heading())
+        val unsortedList = mutableListOf("123", "989", "000", "981", "778", "996")
+        val stats = RuntimeStats()
+        val sortedList = merge_sort(unsortedList, stats)
+        print("sorted list=$sortedList")
+        println(", $stats")
+    }
+
 }
+
+/** O(n * log(n)) */
+fun merge_sort(list: MutableList<String>, stats: RuntimeStats): MutableList<String> {
+    // Can't split lists anymore, so stop recursion
+    val length = list.size
+    if (length <= 1) return list
+
+    // Split the list into two and recurse (divide)
+    val middleIndex = length / 2
+    val leftList = merge_sort(list.subList(0, middleIndex), stats)
+    val rightList = merge_sort(list.subList(middleIndex, length), stats)
+
+    // Merge the left and right lists (conquer)
+    return merge(leftList, rightList)
+}
+
+fun merge(leftList: MutableList<String>, rightList: MutableList<String>): MutableList<String> {
+    val result = mutableListOf<String>()
+    var leftIndex = 0
+    var rightIndex = 0
+    while (leftIndex < leftList.size && rightIndex < rightList.size) {
+        val lhs = leftList[leftIndex]
+        val rhs = rightList[rightIndex]
+        if (lhs < rhs) {
+            result.add(lhs)
+            leftIndex++
+        } else {
+            result.add(rhs)
+            rightIndex++
+        }
+    }
+
+    // Copy remaining elements of leftList (if any) into the result
+    while (leftIndex < leftList.size) {
+        result.add(leftList[leftIndex])
+        leftIndex++
+    }
+    // Copy remaining elements of rightList (if any) into the result
+    while (rightIndex < rightList.size) {
+        result.add(rightList[rightIndex])
+        rightIndex++
+    }
+    return result
+}
+
 
 /** O(n^2) */
 fun insertion_sort(list: MutableList<String>, stats: RuntimeStats) {
