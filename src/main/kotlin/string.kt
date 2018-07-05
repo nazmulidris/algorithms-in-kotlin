fae0807140e2a9d5128448db2e168dd56da4f694
Nazmul Idris <nazmul@fasterlap.com>
2018-07-04 21:26:53 -0700

    Add more functionality to the substring match function
    - Add RuntimeStats to both the optimized and unoptimized versions
    - Change the optimizing function to match multiple occurences of a
      substring with O(n) performance

--- src/main/kotlin/string.kt
+++ src/main/kotlin/string.kt
@@ -21,17 +21,25 @@ import utils.RuntimeStats
 import utils.heading
 
 fun main(args: Array<String>) {
+    println("substring".heading())
+
+    val arg1 = "Hello world".toCharArray()
+    val arg2 = "o".toCharArray()
+
     with(RuntimeStats()) {
-        println("substring".heading())
-        val arg1 = "Hello world".toCharArray()
-        var arg2 = "o".toCharArray()
-        println("substring(\n\t${arg1.joinToString("・")}, " +
-                        "\n\t${arg2.joinToString("・")}" +
-                        "\n) = ${substring(arg1, arg2)}")
-        println("substring_optimized(\n\t${arg1.joinToString("・")}, " +
-                        "\n\t${arg2.joinToString("・")}" +
-                        "\n) = ${substring_optimized(arg1, arg2)}")
+        print("substring(\n\t${arg1.joinToString("・")}, " +
+                      "\n\t${arg2.joinToString("・")}" +
+                      "\n) = ${substring(arg1, arg2, this)}")
+        println(", $this")
     }
+
+    with(RuntimeStats()) {
+        print("substring_optimized(\n\t${arg1.joinToString("・")}, " +
+                      "\n\t${arg2.joinToString("・")}" +
+                      "\n) = ${substring_optimized(arg1, arg2, this)}")
+        println(", $this")
+    }
+
 }
 
 /**
@@ -39,7 +47,7 @@ fun main(args: Array<String>) {
  *
  * This is an inefficient brute force algorithm which has quadratic complexity.
  */
-fun substring(str: CharArray, substr: CharArray): Any {
+fun substring(str: CharArray, substr: CharArray, stats: RuntimeStats): Any {
     // substr can't be longer than str
     if (substr.size > str.size) return false
 
@@ -47,13 +55,16 @@ fun substring(str: CharArray, substr: CharArray): Any {
     // to see if matches exist for substr
     var occurrences = 0
     for (cursor1 in 0 until str.size) {
+        stats.operations++
         var matchCount = 0
         for (cursor2 in 0 until substr.size) {
+            stats.operations++
             val index = cursor1 + cursor2
             // If index exceeds the size of str that means substr wasn't found
             if (index > str.size - 1) break
             // If there's a match at index between the str and substr then remember it
             if (str[index] == substr[cursor2]) matchCount++
+            stats.comparisons++
         }
         // Found a match
         if (matchCount == substr.size) occurrences++
@@ -75,22 +86,41 @@ fun substring(str: CharArray, substr: CharArray): Any {
  * This function uses a deterministic finite automation (DFA) method which entails the use of a
  * state machine to keep track of progress in a game.
  */
-fun substring_optimized(str: CharArray, substr: CharArray): Boolean {
+fun substring_optimized(str: CharArray, substr: CharArray, stats: RuntimeStats): Any {
 
     class StateMachine(val pattern: CharArray) {
         var cursor = 0
         fun add(character: Char) {
+            stats.comparisons++
             if (pattern[cursor] == character) cursor++
             else cursor = 0
         }
 
         fun isMatch() = cursor == pattern.size
+        fun reset() {
+            cursor = 0
+        }
     }
 
     val stateMachine = StateMachine(substr)
+    var numberOfOccurrences = 0
+
     for (cursor in 0 until str.size) {
+        stats.operations++
         stateMachine.add(str[cursor])
-        if (stateMachine.isMatch()) break
+        if (stateMachine.isMatch()) {
+            stateMachine.reset()
+            numberOfOccurrences++
+        }
     }
-    return stateMachine.isMatch()
+
+    return object {
+        val occurrences = numberOfOccurrences
+        val matchFound = numberOfOccurrences > 0
+        override fun toString(): String = StringBuilder().apply {
+            append("occurrences = $occurrences")
+            append(", matchFound = $matchFound")
+        }.toString()
+    }
+
 }
