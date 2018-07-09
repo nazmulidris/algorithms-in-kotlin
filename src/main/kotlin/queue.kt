f8e39a706b3abcf3a44cd030787885e5dca9d464
Nazmul Idris <nazmul@google.com>
2018-07-09 10:51:23 -0700

    Add optimized implementation of a circular queue (RingBuffer)

--- src/main/kotlin/queue.kt
+++ src/main/kotlin/queue.kt
@@ -21,7 +21,7 @@ import utils.heading
 
 fun main(args: Array<String>) {
     println("circular queue".heading())
-    val queue = CircularQueue<String>(4)
+    val queue = RingBuffer<String>(4)
     println("add 1, ${queue.enqueue("1")}")
     println("add 2, ${queue.enqueue("2")}")
     println("add 3, ${queue.enqueue("3")}")
@@ -49,7 +49,92 @@ fun main(args: Array<String>) {
     println("add 3, ${queue.enqueue("3")}")
 }
 
-class CircularQueue<T>(val maxSize: Int = 10) {
+/**
+ * RingBuffer uses a fixed length array to implement a queue, where:
+ * - [tail] Items are added to the tail
+ * - [head] Items are removed from the head
+ * - [capacity] Keeps track of how many items are currently in the queue
+ *
+ * References
+ * - [More info - tutorial](http://tutorials.jenkov.com/java-performance/ring-buffer.html)
+ * - [More info - video](https://www.youtube.com/watch?v=ia__kyuwGag)
+ */
+class RingBuffer<T>(val maxSize: Int = 10) {
+    val array = mutableListOf<T?>().apply {
+        for (index in 0 until maxSize) {
+            add(null)
+        }
+    }
+
+    // Head - remove from the head (read index)
+    var head = 0
+
+    // Tail - add to the tail (write index)
+    var tail = 0
+
+    // How many items are currently in the queue
+    var capacity = 0
+
+    fun clear() {
+        head = 0
+        tail = 0
+    }
+
+    fun enqueue(item: T): RingBuffer<T> {
+        // Check if there's space before attempting to add the item
+        if (capacity == maxSize) throw OverflowException("Can't add $item, queue is full")
+
+        array[tail] = item
+        // Loop around to the start of the array if there's a need for it
+        tail = (tail + 1) % maxSize
+        capacity++
+
+        return this
+    }
+
+    fun dequeue(): T? {
+        // Check if queue is empty before attempting to remove the item
+        if (capacity == 0) throw UnderflowException("Queue is empty, can't dequeue()")
+
+        val result = array[head]
+        // Loop around to the start of the array if there's a need for it
+        head = (head + 1) % maxSize
+        capacity--
+
+        return result
+    }
+
+    fun peek(): T? = array[head]
+
+    /**
+     * - Ordinarily, T > H ([isNormal]).
+     * - However, when the queue loops over, then T < H ([isFlipped]).
+     */
+    fun isNormal(): Boolean {
+        return tail > head
+    }
+
+    fun isFlipped(): Boolean {
+        return tail < head
+    }
+
+    override fun toString(): String = StringBuffer().apply {
+        var itemCount = capacity
+        val items = mutableListOf<T?>()
+        var readIndex = head
+        while (itemCount > 0) {
+            items.add(array[readIndex])
+            readIndex = (readIndex + 1) % maxSize
+            itemCount--
+        }
+        append(items.joinToString(", ",
+                                  "{",
+                                  "}").blue()).append(" [capacity = $capacity, H = $head, T = $tail]".yellow())
+    }.toString()
+
+}
+
+class CircularQueueNaive<T>(val maxSize: Int = 10) {
     val array = mutableListOf<T?>().apply {
         for (index in 0 until maxSize) {
             add(null)
@@ -65,13 +150,13 @@ class CircularQueue<T>(val maxSize: Int = 10) {
     // How many items are currently in the queue
     var size = 0
 
-    fun enqueue(item: T): CircularQueue<T> {
+    fun enqueue(item: T): CircularQueueNaive<T> {
         // Check if there's space before attempting to add the item
         if (size == maxSize) throw OverflowException("Can't add $item, queue is full")
 
         // Loop around to the start of the array if there's a need for it
         if (tail == maxSize) {
-            println("\tLooping head to the start of the array".red())
+            println("\tLooping tail to the start of the array".red())
             tail = 0
         }
 
