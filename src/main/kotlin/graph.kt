49b019cfcef37bef866622987cf93dab4551f833
Nazmul Idris <nazmul@fasterlap.com>
2018-11-26 16:13:19 -0800

    Update comments

--- src/main/kotlin/graph.kt
+++ src/main/kotlin/graph.kt
@@ -61,10 +61,10 @@ class Graph<T> {
     val adjacencyMap: HashMap<T, HashSet<T>> = HashMap()
 
     fun addEdge(sourceVertex: T, destinationVertex: T) {
-        // Add edge to source vertex.
+        // Add edge to source vertex / node.
         adjacencyMap.computeIfAbsent(sourceVertex) { HashSet() }
                 .add(destinationVertex)
-        // Add edge to destination vertex.
+        // Add edge to destination vertex / node.
         adjacencyMap.computeIfAbsent(destinationVertex) { HashSet() }
                 .add(sourceVertex)
     }
